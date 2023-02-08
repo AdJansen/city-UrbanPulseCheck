936631c723379867f752526d82c1b5d1ca8e4f17
AdJansen <45954995+AdJansen@users.noreply.github.com>
2023-02-07 19:30:40 -0500

    Added a sticky searchbar with placeholder text and css

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,1 +1,28 @@
-import react from 'react';
+import React, { Fragment } from "react";
+import './Searchbar.css';
+import { TextField } from '@mui/material';;
+
+
+const Searchbar = () => {
+//   const guestLinks = (
+//     <ul>
+//       <li>
+//         <Link to="/">
+//           <Button className="btn btn-primary createNew">Create New</Button>
+//         </Link>
+//       </li>
+//       <li>
+//         <Link to="/about">
+//           <Button className="btn btn-danger signOut">Sign out</Button>
+//         </Link>
+//       </li>
+//     </ul>
+//   );
+  return (
+    <nav className="navbar">
+      <TextField fullWidth id="outlined-basic" label="City Search" variant="outlined" className="searchField"/>
+    </nav>
+  );
+};
+
+export default Searchbar;
