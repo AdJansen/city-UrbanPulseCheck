7e86a4eff6cc6a52c1810eb60eaec05855f18b45
AdJansen <45954995+AdJansen@users.noreply.github.com>
2023-02-10 16:20:25 -0500

    ARGH

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Searchbar from './Components/Searchbar';
 import { useState, useEffect } from 'react';
 import './App.css';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import Card from '@mui/material/Card';
+import { CardHeader } from '@mui/material';
 
 
 function App() {
@@ -9,14 +12,16 @@ function App() {
   const [urbanDetails, setUrbanDetails] = useState([]);
   const [urbanScores, setUrbanScores] = useState([]);
 
+  const scoreNameIndex = {"Housing":0, "Cost of Living":1, "Startups":2, "Venture Capital":3, "Travel Connectivity":4, "Commute":5, "Business Freedom":6, "Safety":7, "Healthcare":8, "Education":9, "Environmental Quality":10, "Economy":11, "Taxation":12, "Internet Access":13, "Leisure & Culture":14, "Tolerance":15, "Outdoors":16};
+
   useEffect(() => {
     const fetchDetailData = async () => {
       const response = await fetch(`https://api.teleport.org/api/urban_areas/slug:${selectedUrbanArea.toLowerCase().replace(/[\s,]+/g, '-')}/details/`);
       const data = await response.json();
       console.log("Detail Data")
       console.log(data.categories)
-      setUrbanDetails(data.categories); 
-      
+      setUrbanDetails(data.categories);
+
     };
     const fetchScoreData = async () => {
       const response = await fetch(`https://api.teleport.org/api/urban_areas/slug:${selectedUrbanArea.toLowerCase().replace(/[\s,]+/g, '-')}/scores/`);
@@ -34,17 +39,25 @@ function App() {
   return (
     <div className="App">
       <Searchbar
-        selectedUrbanArea={selectedUrbanArea} 
+        selectedUrbanArea={selectedUrbanArea}
         setSelectedUrbanArea={setSelectedUrbanArea}
       />
       <div className='mainWindow'>
-        {urbanDetails.map((urbanDetail) => (
-          <div className='urbanDetail'>
-            <h1>{urbanDetail.label}</h1>
-          </div>)
-        )}
-        
-          
+        <Grid2 container spacing={2} className="BrowseCards">
+          {
+            urbanDetails.map((urbanDetail) => (
+              
+              <Grid2 xs={12} sm={6} md={4} lg={3} key={urbanDetail.label}>
+                <Card variant="outlined">
+                  
+                  <CardHeader title={urbanDetail.label} subheader={urbanScores[scoreNameIndex[urbanDetail.label]].name}/>
+                </Card>
+              </Grid2>
+            ))
+          }
+        </Grid2>
+
+
       </div>
 
     </div>
