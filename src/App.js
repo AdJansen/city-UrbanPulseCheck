330b16033a23c626221ebd5b63e60bf2a7ac328f
AdJansen <45954995+AdJansen@users.noreply.github.com>
2023-02-10 16:40:14 -0500

    Card Content added :)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import Card from '@mui/material/Card';
-import { CardHeader } from '@mui/material';
+import { CardHeader, Typography, CardContent } from '@mui/material';
 
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
   const [urbanDetails, setUrbanDetails] = useState([]);
   const [urbanScores, setUrbanScores] = useState([]);
 
-  const scoreNameIndex = {"Housing":0, "Cost of Living":1, "Startups":2, "Venture Capital":3, "Travel Connectivity":4, "Commute":5, "Business Freedom":6, "Safety":7, "Healthcare":8, "Education":9, "Environmental Quality":10, "Economy":11, "Taxation":12, "Internet Access":13, "Leisure & Culture":14, "Tolerance":15, "Outdoors":16};
+  const scoreNameIndex = { "Housing": 0, "Cost of Living": 1, "Startups": 2, "Venture Capital": 3, "Travel Connectivity": 4, "Commute": 5, "Business Freedom": 6, "Safety": 7, "Healthcare": 8, "Education": 9, "Environmental Quality": 10, "Economy": 11, "Taxation": 12, "Internet Access": 13, "Leisure & Culture": 14, "Tolerance": 15, "Outdoors": 16 };
 
   useEffect(() => {
     const fetchDetailData = async () => {
@@ -35,7 +35,7 @@ function App() {
   }, [selectedUrbanArea]);
 
 
-
+  // urbanScore details are broken. Need to be able to access the data in the array
   return (
     <div className="App">
       <Searchbar
@@ -46,11 +46,19 @@ function App() {
         <Grid2 container spacing={2} className="BrowseCards">
           {
             urbanDetails.map((urbanDetail) => (
-              
+
               <Grid2 xs={12} sm={6} md={4} lg={3} key={urbanDetail.label}>
                 <Card variant="outlined">
-                  
-                  <CardHeader title={urbanDetail.label} subheader={urbanScores[scoreNameIndex[urbanDetail.label]].name}/>
+
+                  <CardHeader title={urbanDetail.label} />
+
+                  <CardContent>
+                    {urbanDetail.data.map((data) => (
+                      <Typography key={data.label} variant="body2" color="text.secondary">
+                        {data.label}: {data.string_value}{data.float_value}{data.int_value}{data.currency_dollar_value}
+                      </Typography>
+                    ))}
+                  </CardContent>
                 </Card>
               </Grid2>
             ))
