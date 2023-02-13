503628e59dd03ac12b740986809a6b9e464cb19e
AdJansen <45954995+AdJansen@users.noreply.github.com>
2023-02-12 21:01:08 -0500

    Added score data sending to Cards & score display

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import Searchbar from './Components/Searchbar';
 import { useState, useEffect } from 'react';
 import './App.css';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
-import Card from '@mui/material/Card';
-import { CardHeader, Typography, CardContent } from '@mui/material';
 import CityCard from './Components/CityCard';
 
 
@@ -14,6 +12,7 @@ function App() {
   const [urbanScores, setUrbanScores] = useState([]);
 
   const scoreNameIndex = { "Housing": 0, "Cost of Living": 1, "Startups": 2, "Venture Capital": 3, "Travel Connectivity": 4, "Commute": 5, "Business Freedom": 6, "Safety": 7, "Healthcare": 8, "Education": 9, "Environmental Quality": 10, "Economy": 11, "Taxation": 12, "Internet Access": 13, "Leisure & Culture": 14, "Tolerance": 15, "Outdoors": 16 };
+
 
   useEffect(() => {
     const fetchDetailData = async () => {
@@ -44,15 +43,21 @@ function App() {
         setSelectedUrbanArea={setSelectedUrbanArea}
       />
       <div className='mainWindow'>
-        <div dangerouslySetInnerHTML={{__html:urbanScores.summary}} /* Displays the City Summary, might want to change*//> 
+        <div dangerouslySetInnerHTML={{ __html: urbanScores.summary }} /* Displays the City Summary, might want to change*/ />
 
         <Grid2 container spacing={2} className="BrowseCards">
           {
-            urbanDetails.map((urbanDetail) => (
-              <Grid2 xs={12} sm={6} md={4} lg={3} key={urbanDetail.label}>
-                <CityCard urbanDetail={urbanDetail} />
-              </Grid2>
-            ))
+            urbanDetails && urbanScores ? //This ternary operator is used to check if the data has been loaded before trying to render it
+              urbanDetails.map((urbanDetail) => ( //This is the map function that iterates through the data and renders the cards, each card is passed on category of city data, and the corresponding score
+                <Grid2 xs={12} sm={6} md={4} lg={3}> 
+                  <CityCard 
+                    urbanDetail={urbanDetail} 
+                    urbanScore={urbanScores.categories ? urbanScores.categories[scoreNameIndex[urbanDetail.label]] : undefined} 
+                    
+                  />
+                </Grid2>
+              ))
+              : null
           }
         </Grid2>
 
--- src/Components/CityCard.js
+++ src/Components/CityCard.js
@@ -4,7 +4,6 @@ import React from 'react';
 import { styled } from '@mui/material/styles';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Typography from '@mui/material/Typography';
 import { useState } from 'react';
 // import ExpandMore from '@mui/icons-material/ExpandMore';
 import IconButton from '@mui/material/IconButton';
@@ -24,18 +23,21 @@ const ExpandMore = styled((props) => { //ExapandMore was taken from MUI
 
 //Card component taken from material-ui with some modifications to work with our data
 //CityCard will probably need to intake data from the API, and then display it in the card
-const CityCard = ({ urbanDetail }) => {
+const CityCard = ({ urbanDetail, urbanScore }) => {
     const [expanded, setExpanded] = useState(false);
 
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
-
+    console.log(urbanScore ? urbanScore.score_out_of_10 : null)
     return (
 
         <Card >
 
-            <CardHeader title={urbanDetail.label} />
+            <CardHeader
+                title={urbanDetail.label}
+                subheader= {urbanScore ? "Teleport Score Out of 10: "+urbanScore.score_out_of_10 : null}
+            />
 
 
             <CardActions disableSpacing>
@@ -55,15 +57,15 @@ const CityCard = ({ urbanDetail }) => {
                         //     {data.label} :{data.string_value}{data.float_value}{data.int_value}{data.currency_dollar_value}{data.percent_value}
                         // </Typography>
                         <div style={{ display: "flex", justifyContent: "space-between" }}>
-                        <div style={{ textAlign: "left" }}>{data.label} :</div>
-                        <div style={{ textAlign: "right" }}>
-                            {data.string_value ? data.string_value : null}
-                            {data.float_value ? data.float_value : null}
-                            {data.int_value ? data.int_value : null}
-                            {data.currency_dollar_value ? data.currency_dollar_value : null}
-                            {data.percent_value ? data.percent_value*100 : null}
+                            <div style={{ textAlign: "left" }}>{data.label} :</div>
+                            <div style={{ textAlign: "right" }}>
+                                {data.string_value ? data.string_value : null}
+                                {data.float_value ? data.float_value : null}
+                                {data.int_value ? data.int_value : null}
+                                {data.currency_dollar_value ? data.currency_dollar_value : null}
+                                {data.percent_value ? data.percent_value * 100 : null}
                             </div>
-                      </div>
+                        </div>
                     ))}
                 </CardContent>
             </Collapse>
